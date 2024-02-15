0083e05534065d4fe78532c2e428562459a24c34
Thiago Batista <cavalcantibatista@gmail.com>
2024-02-15 22:14:38 +0000

    First test

---
+++ src/main/java/com/thiago/restapi/RestapiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/restapi/RestapiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
